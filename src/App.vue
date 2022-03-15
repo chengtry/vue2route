b99898b13bffc21482a0246fa5671c12ee1883fa
chengtry <412307724@qq.com>
2022-03-15 18:14:12 +0800

    二级路由页面Demo

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="row">
       <div class="col-xs-offset-2 col-xs-8">
-        <div class="page-header"><h2>Vue Router Demo</h2></div>
+        <div class="page-header"><h2>Vue2 Router Demo</h2></div>
       </div>
     </div>
     <div class="row">
--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,30 @@ import VueRouter from 'vue-router'
 //引入组件
 import About from '../components/About'
 import Home from '../components/Home'
+import Message from '../pages/Message'
+import News from '../pages/News'
 
 
 //创建并暴露一个路由器
 export default new VueRouter({
-    routes:[
+    routes: [
         {
-            path:'/about',
-            component:About
+            path: '/about',
+            component: About
         },
         {
-            path:'/home',
-            component:Home
+            path: '/home',
+            component: Home,
+            children: [
+                {
+                    path: 'message',
+                    component: Message
+                },
+                {
+                    path: 'news',
+                    component: News
+                }
+            ]
         },
     ]
 })
